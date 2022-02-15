9ec3ea2b89fad9ceedd6d300d25e4391625cddd7
junglelin <jungle0803@gmail.com>
2022-02-15 16:43:53 +0800

    Add gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,12 @@
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/jj-nexifytw/'
+        }
+      }
+    : {}
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
